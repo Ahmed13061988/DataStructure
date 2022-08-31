31871e3081e52bea7d6e79bf43d5527ad61eec41
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-31 11:54:16 -0600

    adding element to the mainMenu array by creating newMenu array and using spread operator

--- script.js
+++ script.js
@@ -118,3 +118,8 @@ const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
 const newArr = [1, 2, ...arr];
 console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, `Gnocci`];
+
+console.log(newMenu);
